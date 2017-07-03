0e7779bb53f11641899c7e7c06d702ccafb3e72f
tommy <hingory@qq.com>
2017-07-03 18:09:14 +0800

    1

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+var drowm=require('./dropdown.js');
+var carousel=require('./carousel.js');
+var transiton=require('./transition.js');
+require('../css/bootstrap.min.css');
+require('../css/reset.css');
+require('../css/main.css');

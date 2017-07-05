350be5a9467f064d99425c0199c82d75867c0199
tommy <hingory@qq.com>
2017-07-05 18:56:02 +0800

    10

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,5 @@
-var transiton=require('./js/components/transition.js');
-var carousel=require('./js/components/carousel.js');
+require('./js/components/transition.js');
+require('./js/components/carousel.js');
 require('./js/components/dropdown.js');
 require('./js/index.js');
 require('./css/bootstrap.min.css');

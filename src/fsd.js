6c06091d1ff6985c8eb8774beb98535eab421173
tommy <hingory@qq.com>
2017-08-12 19:15:52 +0800

    1

---
+++ src/fsd.js
@@ -0,0 +1,276 @@
+{
+    "code": 2000,
+    "data": {
+    "allcomment": [
+        {
+            "id": 3,
+            "cornet": 100000,
+            "pid": 0,
+            "c_time": 1502446589,
+            "content": "333333",
+            "nickname": "arroy",
+            "like": 0,
+            "report": 2
+        },
+        {
+            "id": 2,
+            "cornet": 100000,
+            "pid": 0,
+            "c_time": 1502445577,
+            "content": "222222",
+            "nickname": "arroy",
+            "like": 11,
+            "report": 1,
+            "_child": [
+                {
+                    "id": 12,
+                    "cornet": 100000,
+                    "pid": 2,
+                    "c_time": 1502446632,
+                    "content": "VVVVVVV",
+                    "nickname": "arroy",
+                    "like": 0,
+                    "report": 0,
+                    "_child": [
+                        {
+                            "id": 16,
+                            "cornet": 100000,
+                            "pid": 12,
+                            "c_time": 1502505124,
+                            "content": "12121212",
+                            "nickname": "arroy",
+                            "like": 0,
+                            "report": 0
+                        }
+                    ]
+                },
+                {
+                    "id": 11,
+                    "cornet": 100000,
+                    "pid": 2,
+                    "c_time": 1502446630,
+                    "content": "BBBBBBB",
+                    "nickname": "arroy",
+                    "like": 0,
+                    "report": 0
+                }
+            ]
+        },
+        {
+            "id": 1,
+            "cornet": 100000,
+            "pid": 0,
+            "c_time": 1502445559,
+            "content": "avcz",
+            "nickname": "arroy",
+            "like": 3,
+            "report": 3,
+            "_child": [
+                {
+                    "id": 9,
+                    "cornet": 100000,
+                    "pid": 1,
+                    "c_time": 1502446617,
+                    "content": "ffffff",
+                    "nickname": "arroy",
+                    "like": 0,
+                    "report": 0
+                },
+                {
+                    "id": 8,
+                    "cornet": 100000,
+                    "pid": 1,
+                    "c_time": 1502446615,
+                    "content": "eeeeee",
+                    "nickname": "arroy",
+                    "like": 0,
+                    "report": 0
+                },
+                {
+                    "id": 7,
+                    "cornet": 100000,
+                    "pid": 1,
+                    "c_time": 1502446612,
+                    "content": "ddddddd",
+                    "nickname": "arroy",
+                    "like": 0,
+                    "report": 0
+                },
+                {
+                    "id": 6,
+                    "cornet": 100000,
+                    "pid": 1,
+                    "c_time": 1502446610,
+                    "content": "cccccc",
+                    "nickname": "arroy",
+                    "like": 0,
+                    "report": 0
+                },
+                {
+                    "id": 5,
+                    "cornet": 100000,
+                    "pid": 1,
+                    "c_time": 1502446607,
+                    "content": "bbbbbb",
+                    "nickname": "arroy",
+                    "like": 0,
+                    "report": 0
+                },
+                {
+                    "id": 4,
+                    "cornet": 100000,
+                    "pid": 1,
+                    "c_time": 1502446601,
+                    "content": "aaaaaa",
+                    "nickname": "arroy",
+                    "like": 0,
+                    "report": 0,
+                    "_child": [
+                        {
+                            "id": 15,
+                            "cornet": 100000,
+                            "pid": 4,
+                            "c_time": 1502502734,
+                            "content": "add4444",
+                            "nickname": "arroy",
+                            "like": 0,
+                            "report": 0
+                        }
+                    ]
+                }
+            ]
+        }
+    ],
+        "hotcomment": [
+        {
+            "id": 2,
+            "cornet": 100000,
+            "pid": 0,
+            "c_time": 1502445577,
+            "content": "222222",
+            "nickname": "arroy",
+            "like": 11,
+            "report": 1,
+            "_child": [
+                {
+                    "id": 11,
+                    "cornet": 100000,
+                    "pid": 2,
+                    "c_time": 1502446630,
+                    "content": "BBBBBBB",
+                    "nickname": "arroy",
+                    "like": 0,
+                    "report": 0
+                },
+                {
+                    "id": 12,
+                    "cornet": 100000,
+                    "pid": 2,
+                    "c_time": 1502446632,
+                    "content": "VVVVVVV",
+                    "nickname": "arroy",
+                    "like": 0,
+                    "report": 0,
+                    "_child": [
+                        {
+                            "id": 16,
+                            "cornet": 100000,
+                            "pid": 12,
+                            "c_time": 1502505124,
+                            "content": "12121212",
+                            "nickname": "arroy",
+                            "like": 0,
+                            "report": 0
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            "id": 1,
+            "cornet": 100000,
+            "pid": 0,
+            "c_time": 1502445559,
+            "content": "avcz",
+            "nickname": "arroy",
+            "like": 3,
+            "report": 3,
+            "_child": [
+                {
+                    "id": 4,
+                    "cornet": 100000,
+                    "pid": 1,
+                    "c_time": 1502446601,
+                    "content": "aaaaaa",
+                    "nickname": "arroy",
+                    "like": 0,
+                    "report": 0,
+                    "_child": [
+                        {
+                            "id": 15,
+                            "cornet": 100000,
+                            "pid": 4,
+                            "c_time": 1502502734,
+                            "content": "add4444",
+                            "nickname": "arroy",
+                            "like": 0,
+                            "report": 0
+                        }
+                    ]
+                },
+                {
+                    "id": 5,
+                    "cornet": 100000,
+                    "pid": 1,
+                    "c_time": 1502446607,
+                    "content": "bbbbbb",
+                    "nickname": "arroy",
+                    "like": 0,
+                    "report": 0
+                },
+                {
+                    "id": 6,
+                    "cornet": 100000,
+                    "pid": 1,
+                    "c_time": 1502446610,
+                    "content": "cccccc",
+                    "nickname": "arroy",
+                    "like": 0,
+                    "report": 0
+                },
+                {
+                    "id": 7,
+                    "cornet": 100000,
+                    "pid": 1,
+                    "c_time": 1502446612,
+                    "content": "ddddddd",
+                    "nickname": "arroy",
+                    "like": 0,
+                    "report": 0
+                },
+                {
+                    "id": 8,
+                    "cornet": 100000,
+                    "pid": 1,
+                    "c_time": 1502446615,
+                    "content": "eeeeee",
+                    "nickname": "arroy",
+                    "like": 0,
+                    "report": 0
+                },
+                {
+                    "id": 9,
+                    "cornet": 100000,
+                    "pid": 1,
+                    "c_time": 1502446617,
+                    "content": "ffffff",
+                    "nickname": "arroy",
+                    "like": 0,
+                    "report": 0
+                }
+            ]
+        }
+    ]
+},
+    "time": 1502536373
+}
